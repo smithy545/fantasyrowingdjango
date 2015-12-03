018bf5f6195b03eb1bc239359e9e726702ee529e
smithy545 <smithy545@gmail.com>
2015-12-03 10:32:14 -0500

    Removed the not used folder from repository

--- _notused/viewteam/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Team
-
-class TeamAdmin(admin.ModelAdmin):
-    fields = ["user", "name", "team_members"]
-    filter_horizontal = ("team_members",)
-
-admin.site.register(Team, TeamAdmin)
